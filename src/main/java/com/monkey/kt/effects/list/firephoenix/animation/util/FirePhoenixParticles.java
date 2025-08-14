17c0999a208026a04d117f36b64b3396bbc970fb
MonkeyMoon104 <lollolilo32@gmail.com>
2025-08-14 18:58:02 +0200

    add phoenix look always at killer

--- src/main/java/com/monkey/kt/effects/list/firephoenix/animation/util/FirePhoenixParticles.java
+++ src/main/java/com/monkey/kt/effects/list/firephoenix/animation/util/FirePhoenixParticles.java
@@ -7,6 +7,7 @@ import org.bukkit.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class FirePhoenixParticles {
 
@@ -55,6 +56,9 @@ public class FirePhoenixParticles {
 
             @Override
             public void run() {
+                Vector directionToKiller = killer.getLocation().toVector().subtract(center.toVector()).normalize();
+                double yawToKiller = Math.atan2(-directionToKiller.getX(), directionToKiller.getZ()) + Math.PI;
+                double pitchToKiller = Math.asin(-directionToKiller.getY());
                 if (step > 240) {
                     world.spawnParticle(Particle.FLASH, center, 3);
                     world.spawnParticle(Particle.EXPLOSION_LARGE, center, 5, 2, 2, 2, 0);
@@ -75,7 +79,7 @@ public class FirePhoenixParticles {
                     return;
                 }
 
-                double forwardTilt = Math.toRadians(25);
+                double forwardTilt = Math.toRadians(25) + pitchToKiller;
                 double phoenixMove = Math.sin(step * 0.1) * 0.3;
 
                 for (double y = 0; y <= 4; y += 0.12) {
@@ -86,12 +90,16 @@ public class FirePhoenixParticles {
                     for (double angle = 0; angle < Math.PI * 2; angle += Math.PI / 12) {
                         double x = Math.cos(angle) * bodyRadius;
                         double z = Math.sin(angle) * bodyRadius + tiltedZ;
-                        spawnDust(world, center.clone().add(x, tiltedY + phoenixMove, z),
+
+                        double rotatedX = x * Math.cos(yawToKiller) - z * Math.sin(yawToKiller);
+                        double rotatedZ = x * Math.sin(yawToKiller) + z * Math.cos(yawToKiller);
+
+                        spawnDust(world, center.clone().add(rotatedX, tiltedY + phoenixMove, rotatedZ),
                                 phoenixColors[step % phoenixColors.length], 1.6f);
 
                         if (Math.random() < 0.1) {
                             world.spawnParticle(Particle.FLAME,
-                                    center.clone().add(x, tiltedY + phoenixMove, z),
+                                    center.clone().add(rotatedX, tiltedY + phoenixMove, rotatedZ),
                                     0, 0.05, 0, 0, 0.02);
                         }
                     }
@@ -99,7 +107,11 @@ public class FirePhoenixParticles {
 
                 double headY = 4.5 * Math.cos(forwardTilt) + phoenixMove;
                 double headZ = -4.5 * Math.sin(forwardTilt);
-                Location headPos = center.clone().add(0, headY, headZ);
+
+                double rotatedHeadX = 0 * Math.cos(yawToKiller) - headZ * Math.sin(yawToKiller);
+                double rotatedHeadZ = 0 * Math.sin(yawToKiller) + headZ * Math.cos(yawToKiller);
+
+                Location headPos = center.clone().add(rotatedHeadX, headY, rotatedHeadZ);
 
                 spawnSphere(world, headPos, 0.4, Particle.FLAME);
                 spawnSphere(world, headPos, 0.25, phoenixColors[0], 1.8f);
@@ -117,15 +129,22 @@ public class FirePhoenixParticles {
                     }
                 }
 
+                double beakX = 0 * Math.cos(yawToKiller) - (-0.4) * Math.sin(yawToKiller);
+                double beakZ = 0 * Math.sin(yawToKiller) + (-0.4) * Math.cos(yawToKiller);
+
                 world.spawnParticle(Particle.REDSTONE,
-                        headPos.clone().add(0, 0, -0.4),
+                        headPos.clone().add(beakX, 0, beakZ),
                         0, 0, 0, 0, new Particle.DustOptions(Color.fromRGB(200, 100, 0), 1.0f));
 
                 for (int i = 0; i < 5; i++) {
                     double crownX = (Math.random() - 0.5) * 0.6;
                     double crownZ = (Math.random() - 0.5) * 0.4;
+
+                    double rotatedCrownX = crownX * Math.cos(yawToKiller) - crownZ * Math.sin(yawToKiller);
+                    double rotatedCrownZ = crownX * Math.sin(yawToKiller) + crownZ * Math.cos(yawToKiller);
+
                     world.spawnParticle(Particle.FLAME,
-                            headPos.clone().add(crownX, 0.3 + Math.random() * 0.8, crownZ),
+                            headPos.clone().add(rotatedCrownX, 0.3 + Math.random() * 0.8, rotatedCrownZ),
                             0, 0, 0, 0, 0.01);
                 }
 
@@ -137,14 +156,11 @@ public class FirePhoenixParticles {
                         for (double wingLength = 0.5; wingLength < 7; wingLength += 0.3) {
 
                             double wavePhase = wingBeatSpeed - (wingLength * 0.4);
-
                             double wingBeat = Math.sin(wavePhase) * wingAmplitude;
-
                             double amplitudeReduction = 1.0 - (wingLength / 7.0) * 0.3;
                             wingBeat *= amplitudeReduction;
 
                             double wingY = 2 + wingBeat;
-
                             double wingTiltY = wingY * Math.cos(forwardTilt);
                             double wingTiltZ = -wingY * Math.sin(forwardTilt) * 0.3;
 
@@ -152,10 +168,13 @@ public class FirePhoenixParticles {
                             double wingX = side * (wingLength + wingCurve);
                             double wingZ = wingTiltZ + Math.cos(wingLength / 7 * Math.PI) * 1.2;
 
+                            double rotatedWingX = wingX * Math.cos(yawToKiller) - wingZ * Math.sin(yawToKiller);
+                            double rotatedWingZ = wingX * Math.sin(yawToKiller) + wingZ * Math.cos(yawToKiller);
+
                             double featherMovement = Math.sin(wavePhase + wingSection * Math.PI) * 0.15;
                             double featherY = wingTiltY + featherMovement;
 
-                            Location wingPos = center.clone().add(wingX, featherY, wingZ);
+                            Location wingPos = center.clone().add(rotatedWingX, featherY, rotatedWingZ);
                             spawnDust(world, wingPos,
                                     phoenixColors[(step + (int)(wingLength * 3)) % phoenixColors.length], 1.4f);
 
@@ -182,7 +201,10 @@ public class FirePhoenixParticles {
                         double tiltedTailY = tailY * Math.cos(forwardTilt) - tailZ * Math.sin(forwardTilt);
                         double tiltedTailZ = tailY * Math.sin(forwardTilt) + tailZ * Math.cos(forwardTilt);
 
-                        Location tailPos = center.clone().add(tailX, tiltedTailY, tiltedTailZ);
+                        double rotatedTailX = tailX * Math.cos(yawToKiller) - tiltedTailZ * Math.sin(yawToKiller);
+                        double rotatedTailZ = tailX * Math.sin(yawToKiller) + tiltedTailZ * Math.cos(yawToKiller);
+
+                        Location tailPos = center.clone().add(rotatedTailX, tiltedTailY, rotatedTailZ);
                         spawnDust(world, tailPos,
                                 phoenixColors[(step + (int)(r * 15)) % phoenixColors.length], 1.2f);
 
@@ -197,8 +219,12 @@ public class FirePhoenixParticles {
                         double trailX = (Math.random() - 0.5) * 2;
                         double trailY = (Math.random() - 0.5) * 6;
                         double trailZ = (Math.random() - 0.5) * 2;
+
+                        double rotatedTrailX = trailX * Math.cos(yawToKiller) - trailZ * Math.sin(yawToKiller);
+                        double rotatedTrailZ = trailX * Math.sin(yawToKiller) + trailZ * Math.cos(yawToKiller);
+
                         world.spawnParticle(Particle.FLAME,
-                                center.clone().add(trailX, trailY, trailZ),
+                                center.clone().add(rotatedTrailX, trailY, rotatedTrailZ),
                                 0, 0, 0, 0, 0.05);
                     }
                 }
